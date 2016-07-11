5892bfcb786605331fde0906defff79817bd3f57
Camille Anelli <camilleanelli32@gmail.com>
2016-07-11 15:23:10 +0200

    add validation for pseudoname

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :pseudoname, presence: true
-  validates :pseudoname, uniqueness: { message: "ce nom est deja utilisé" }
+  validates :pseudoname, uniqueness: { message: "ce nom est deja utilisé" }, format: { with: /\A[a-zA-Z0-9]+\Z/ , message: "Ne doit pas contenir d'espace"}            
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   has_many :statuses
