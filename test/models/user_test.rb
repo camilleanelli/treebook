6f1f543e78a96e3c8cb71d394baad359e24e03b6
Camille Anelli <camilleanelli32@gmail.com>
2016-07-11 15:22:41 +0200

    add test to verify validation of no white spaces

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,9 +2,14 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "a user should enter a firstname" do
+  test "a user should have a profile name whithout spaces" do
     user = User.new
+    user.pseudoname = "pseudoname with spaces"
+
     assert !user.save
+    assert !user.errors[:pseudoname].empty?
+    assert user.errors[:pseudoname].include?("Ne doit pas contenir d'espace")
+
   end
 
 end
