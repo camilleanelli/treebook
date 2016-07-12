bfbed625665d8d7cb267034976bd8dc67422ef80
Camille Anelli <camilleanelli32@gmail.com>
2016-07-12 11:14:45 +0200

    test content status presence

--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'that the status requires content to be valid' do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+
+  test 'that the content is at least to letters long' do
+    status = Status.new
+    status.content = "H"
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
 end
