f2ab8ecb606b49b7eef4be7c212137df95bffe0a
Camille Anelli <camilleanelli32@gmail.com>
2016-07-24 15:57:19 +0200

    add relation for model user friendship

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -1,6 +1,4 @@
 class UserFriendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
-
-  attr_accessible :friend, :user, :friend_id, :user_id
 end
